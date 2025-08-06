5e1c3da1a33d30a3092949a751c6b40eea8ac0e8
Manfred Baraka Josephat <163660029+manfredbaraka33@users.noreply.github.com>
2025-08-06 15:29:56 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,19 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import { Routes,Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Topbar from './components/Topbar';
-import Speech from './pages/Speech';
-import Sentiment from './pages/Sentiment';
-import About from './pages/About';
-
 function App() {
-
   return (
     <>
-      <Topbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/speech' element={<Speech />}/>
-        <Route path='/sentiment' element={<Sentiment />}/>
-        <Route path='/about' element={<About />}/>
-      </Routes>
-        
+      <div style={{
+        backgroundColor: '#f9fafb',
+        padding: '12px',
+        textAlign: 'center',
+        color: '#555',
+        fontStyle: 'italic',
+        borderBottom: '1px solid #ddd',
+        fontSize: '14px',
+      }}>
+        🚧 Our website is currently under enhancement. Thanks for your patience and understanding!
+      </div>
+
     </>
   )
 }
 
-export default App
